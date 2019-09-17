ab1bcb013fe1e0112987f76f1f7bd7787e154ffb
Stephen Upchurch <downchapel@gmail.com>
2019-09-17 20:21:43 +0100

    Push tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,16 @@ PATCH_VERSION=0
 git fetch --tags $GITHUB_REPOSITORY_URL
 
 MOST_RECENT_TAG="$(git tag -l --sort=-version:refname "$MAJOR_VERSION.*.$PATCH_VERSION" | head -n 1)"
+
 if [ -z "$MOST_RECENT_TAG" ]
 then
     NEW_TAG="$MAJOR_VERSION.1.$PATCH_VERSION"
 else
     IFS=. read -a ARRAY <<< "$MOST_RECENT_TAG"
-    NEW_TAG="$MAJOR_VERSION.$((${ARRAY[2]} + 1)).$PATCH_VERSION"
+    NEW_TAG="$MAJOR_VERSION.$((${ARRAY[1]} + 1)).$PATCH_VERSION"
 fi
 
+git tag $NEW_TAG
 git push $GITHUB_REPOSITORY_URL $NEW_TAG
 
 echo ::set-output name=version::$NEW_TAG
